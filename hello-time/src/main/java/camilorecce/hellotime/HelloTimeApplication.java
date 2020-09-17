d13df129281af6a522fbb5a5a07fa25718b3a6fc
camilorecce <camilorecce@gmail.com>
2020-09-17 12:52:30 -0300

    Initial commit

---
+++ hello-time/src/main/java/camilorecce/hellotime/HelloTimeApplication.java
@@ -0,0 +1,13 @@
+package camilorecce.hellotime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloTimeApplication.class, args);
+	}
+
+}
---
+++ hello-time/src/test/java/camilorecce/hellotime/HelloTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package camilorecce.hellotime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
