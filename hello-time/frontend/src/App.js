187efb0dfb819a89c018f4b5865a5a9b4bcf2e71
camilorecce <camilorecce@gmail.com>
2020-09-17 19:21:34 -0300

    Pre-Deploy

--- hello-time/frontend/src/App.js
+++ hello-time/frontend/src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [message, setMessage] = useState();
+
+  useEffect(() => {
+    setInterval(hello, 250);
+  });
+
+  const hello = () => {
+    fetch("/api/hello")
+    .then(response => response.text())
+    .then(m => {setMessage(m)});
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {message}
         </p>
         <a
           className="App-link"
